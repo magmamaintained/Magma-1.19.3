992aa1b5ed75e5856fa6dbb06a6380416f2e714b
Malcolm <malcolm.kohne03@gmail.com>
2023-03-23 18:53:15 +0100

    Update CraftPlayer.java

--- src/main/java/org/bukkit/craftbukkit/v1_19_R3/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_19_R3/entity/CraftPlayer.java
@@ -227,7 +227,7 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
 
     @Override
     public String getPlayerListName() {
-        return getHandle().listName == null ? getName() : CraftChatMessage.fromComponent(getHandle().listName);
+        return getHandle().tabListDisplayName == null ? getName() : CraftChatMessage.fromComponent(getHandle().tabListDisplayName);
     }
 
     @Override
@@ -235,7 +235,7 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
         if (name == null) {
             name = getName();
         }
-        getHandle().listName = name.equals(getName()) ? null : CraftChatMessage.fromStringOrNull(name);
+        getHandle().tabListDisplayName = name.equals(getName()) ? null : CraftChatMessage.fromStringOrNull(name);
         for (ServerPlayer player : (List<ServerPlayer>) server.getHandle().players) {
             if (player.getBukkitEntity().canSee(this)) {
                 player.connection.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.UPDATE_DISPLAY_NAME, getHandle()));
