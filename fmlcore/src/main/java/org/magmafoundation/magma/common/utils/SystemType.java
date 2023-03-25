c9524e546f162d535adfa9e2c5005197bc2552c5
Keir Davis <minecraftfun201@gmail.com>
2023-03-25 19:13:07 +0000

    Updated License year

--- fmlcore/src/main/java/org/magmafoundation/magma/common/utils/SystemType.java
+++ fmlcore/src/main/java/org/magmafoundation/magma/common/utils/SystemType.java
@@ -1,6 +1,6 @@
 /*
- * MohistMC
- * Copyright (C) 2018-2022.
+ * Magma Server
+ * Copyright (C) 2019-2023.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -9,11 +9,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package org.magmafoundation.magma.common.utils;
--- src/main/java/org/magmafoundation/magma/configuration/value/values/ArrayValue.java
+++ src/main/java/org/magmafoundation/magma/configuration/value/values/ArrayValue.java
@@ -1,6 +1,6 @@
 /*
  * Magma Server
- * Copyright (C) 2019-2022.
+ * Copyright (C) 2019-2023.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
