299732e4313c4595bc617071201b95745c997323
JustRed23 <manuel.bormans@gmail.com>
2023-05-16 17:49:32 +0200

    Some more 1.18 ported patches

--- src/main/java/org/bukkit/craftbukkit/v1_19_R2/entity/CraftPlayer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_19_R2/entity/CraftPlayer.java
@@ -115,7 +115,7 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
 
     @Override
     public boolean isOp() {
-        return server.getHandle().isOp(getProfile());
+        return getProfile() != null && server.getHandle().isOp(getProfile());
     }
 
     @Override
